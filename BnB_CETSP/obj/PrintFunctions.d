456ecb1ee74f26130baf6aded68cc60ca9798923
Walton P. Coutinho <walton.coutinho@gmail.com>
2018-04-06 16:29:59 +0100

    fixed bug on folders' structure

---
+++ BnB_CETSP/obj/PrintFunctions.d
@@ -0,0 +1,2 @@
+obj/PrintFunctions.o: src/PrintFunctions.cpp src/PrintFunctions.h src/Data.h \
+ src/util.h
---
+++ BnB_CETSP/obj/SolveSocpCplex.d
@@ -0,0 +1,1 @@
+obj/SolveSocpCplex.o: src/SolveSocpCplex.cpp src/SolveSocpCplex.h src/Data.h
